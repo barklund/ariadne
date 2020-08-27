3a298e4b4e3c2f93955ff1a6757dc65cd0ef2105
Morten Barklund <morten.barklund@gmail.com>
2020-08-27 00:51:29 -0400

    Minor optimizations for es2015 minimal tab variant

--- public/tabs/minimal/es2015/script.js
+++ public/tabs/minimal/es2015/script.js
@@ -1,55 +1,45 @@
-function activateTab(tab, tabs, panels) {
-  // Unselect all tabs
-  tabs.forEach((sibling) => {
-    sibling.tabIndex = -1;
-    sibling.classList.remove("tab--active");
-    sibling.setAttribute("aria-selected", false);
-  });
-  // Hide all panels
-  panels.forEach((panel) => {
-    panel.hidden = true;
-  });
+function activateTab(tab, tabs, togglePanelVisibility) {
+  // Unselect old tab
+  const old = tabs.find(tab => tab.matches('[aria-selected=true]'))
+  old.tabIndex = -1;
+  old.classList.remove("tab--active");
+  old.setAttribute("aria-selected", false);
+  // Hide old panel
+  togglePanelVisibility(old, false);
   // Select this tab
   tab.tabIndex = 0;
   tab.classList.add("tab--active");
   tab.setAttribute("aria-selected", true);
-  tab.focus();
   // Show this tab's panel
-  const tabPanel = panels.find(
-    (p) => p.id === tab.getAttribute("aria-controls")
-  );
-  tabPanel.hidden = false;
+  togglePanelVisibility(tab, true);
 }
-
 function hookTabWidget(tablist) {
+  // Some necessary globals
   const tabs = Array.from(tablist.querySelectorAll("[role=tab]"));
-  const panels = tabs.map((tab) =>
-    document.getElementById(tab.getAttribute("aria-controls"))
-  );
+  const togglePanelVisibility = (tab, isVisible) => {
+    const panelId = tab.getAttribute("aria-controls");
+    const panel = document.getElementById(panelId);
+    panel.hidden = !isVisible;
+  }
   const globalDir = getComputedStyle(tablist).direction === "ltr" ? 1 : -1;
-
   // Make sure direct tab selection works
   tabs.forEach((tab) => {
-    tab.addEventListener("click", () => activateTab(tab, tabs, panels));
+    tab.addEventListener("focus", () => activateTab(tab, tabs, togglePanelVisibility));
   });
-
   // Make sure arrow keys work
   tablist.addEventListener("keydown", (evt) => {
     // Ignore irrelevant key presses
     if (!['ArrowLeft', 'ArrowRight'].includes(evt.key)) {
       return;
     }
-
     // Stop propagation and native behaviour
     evt.stopPropagation();
     evt.preventDefault();
-
-    // Get new index and select the tab
+    // Get new index and focus that tab
     const currentIndex = tabs.findIndex(tab => tab.matches('[aria-selected=true]'));
     const deltaIndex = globalDir * (evt.key === 'ArrowRight' ? 1 : -1);
     const newIndex = Math.max(0, Math.min(tabs.length - 1, currentIndex + deltaIndex));
-    activateTab(tabs[newIndex], tabs, panels);
+    tabs[newIndex].focus();
   });
 }
-
 hookTabWidget(document.querySelector("[role=tablist]"));
